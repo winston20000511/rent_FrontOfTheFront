f283052c4c0036657bbaafd01d7cb843ae6384d7
winston20000511 <winston20000511@gmail.com>
2024-12-09 16:03:11 +0800

    2024/12/9 資料夾劃分

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import MemberCenter from '@/components/memberCenter.vue';
+import MemberCenter from '@/components/User/memberCenter.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 
